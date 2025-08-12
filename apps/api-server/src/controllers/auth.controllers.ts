d5df7962ddcab63897e0db2c6cf8ba0baf40f8a0
HarshalRawal <harshalrawal463@gmail.com>
2025-08-12 18:50:07 +0530

    fixed

--- apps/api-server/src/controllers/auth.controllers.ts
+++ apps/api-server/src/controllers/auth.controllers.ts
@@ -4,6 +4,7 @@ import axios from "axios"
 import {ApiError,asyncHandler, sendSuccess} from "@myorg/shared"
 import { verifyGoogleIdToken } from "../utils/verifyGoogleToken.utils";
 import { DecodedGoogleUser } from "../types/decodedGoogleUser.types";
+import { logger } from "@myorg/shared";
 import { prisma } from "@myorg/shared";
 import AuthProvider from "@shared/db";
 
@@ -46,4 +47,31 @@ export const googleCallback = asyncHandler(async(req:Request,res:Response)=>{
     //   })
     // }
     sendSuccess<DecodedGoogleUser>(res,decodedUser,"google auth success",200);
+})
+
+export const githubeConnect = asyncHandler(async (req: Request, res: Response) => {
+  const redirectUrl = `https://github.com/login/oauth/authorize?client_id=${config.githubClientId}` +
+    `&redirect_uri=${encodeURIComponent(config.githubRedirectURL)}` +
+    `&response_type=code` +
+    `&scope=${config.githubScopes}`;
+
+  res.redirect(redirectUrl);
+});
+
+export const githubToken = asyncHandler(async(req:Request,res:Response)=>{
+  const {code} = req.query;
+  if(!code){
+    throw new ApiError("Missing Authorization",400);
+  }
+  
+  logger.info("Reached githubToken Function")
+  const token = await axios.post("https://github.com/login/oauth/access_token",{
+        client_id:config.githubClientId,
+        client_secret:config.githubClientSecret,
+        code: code,
+        grant_type:"authorization_code",
+        redirect_uri:config.githubRedirectURL
+  });
+  
+  sendSuccess(res,token.data,"github auth success",200);
 })
--- apps/api-server/src/routes/auth.routes.ts
+++ apps/api-server/src/routes/auth.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { googleAuth,googleCallback } from "../controllers/auth.controllers";
+import { googleAuth,googleCallback,githubToken,githubeConnect } from "../controllers/auth.controllers";
  const authRoutes = Router();
  authRoutes.get("/google",googleAuth);
  authRoutes.get("/google/callback",googleCallback);
+ authRoutes.get('/github', githubeConnect)
+ authRoutes.get('/github/callback' , githubToken)
 export default authRoutes
